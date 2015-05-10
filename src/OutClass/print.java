eb4c1ebebce3c7a8618785a72f549f96a2a92fb9
loai <loai@192.168.98.178>
2015-05-10 08:21:51 -0400

    First commit

---
+++ src/OutClass/print.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OutClass;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author loai
+ */
+public class print {
+
+    public void spit() {
+
+        System.out.println("start");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(print.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println("");
+        System.out.println("end");
+
+    }
+
+}
---
+++ src/jout1/JOut1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jout1;
+
+import OutClass.print;
+
+/**
+ *
+ * @author loai
+ */
+public class JOut1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        print p = new print();
+        p.spit();
+    }
+    
+}
