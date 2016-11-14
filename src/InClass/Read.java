5b8c5900c202061db52f968aefbf4bfa7dce47c2
lalsalti@hotmail.com <lalsa@CTO-Dev-Lap>
2016-11-13 20:14:54 -0500

    Add Read class

---
+++ src/InClass/Read.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InClass;
+
+/**
+ *
+ * @author lalsa
+ */
+public class Read {
+    String myString = "";
+    public void ReadString(String s)
+    {
+        myString = s;
+    }
+    
+}
